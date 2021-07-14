4722722cada0971b40545cd137a97ee8c71cba17
Teresa Kamau <51070829+tessa5@users.noreply.github.com>
2021-07-14 20:44:37 +0300

    cleaning the form after submit

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -19,7 +19,10 @@ const ExpenseForm = () => {
         dispatch({
             type: "ADD_EXPENSE",
             payload: expense,
-        })
+        });
+
+        setName('');
+        setCost('');
     };
 
     return(
@@ -30,18 +33,18 @@ const ExpenseForm = () => {
                     <input 
                     required='required'
                     text="text"
-                    className="ml-2 text-center "
+                    className="ml-2 text-center w-48"
                     id='name'
                     value={name}
                     onChange={ (event)=> setName(event.target.value)}
                     ></input>    
                 </div>
-                <div className='block px-4 py-2 text-center text-black text-lg'>
+                <div className='block px-4 py-2 ml-3 text-center text-black text-lg'>
                 <label for="cost">Cost</label>
                     <input 
                     required='required'
                     text="text"
-                    className="ml-2 text-center"
+                    className="ml-2 text-center w-48"
                     id='cost'
                     value={cost}
                     onChange={ (event)=> setCost(event.target.value)}
